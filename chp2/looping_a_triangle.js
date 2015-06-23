f55c7b286407b06fb9d7ecdbd15ff1b265038a02
Minsheng Zheng <nadavidsonzheng@gmail.com>
2015-06-23 17:23:05 -0400

    Added missing file

---
+++ chp2/looping_a_triangle.js
@@ -0,0 +1,9 @@
+// Looping a triangle
+//var sym = "#";
+//for (var i = 0; i < 7; i++) {
+//    console.log(sym);
+//	sym += "#";
+//}
+
+for (var sym = "#"; sym.length <= 7; sym+= "#")
+    console.log(sym)
